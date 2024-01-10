681c80d0ebbf7ec76dffbc1341ecd50c4df6d923
Soranosuke2001 <sorazora1.4@gmail.com>
2024-01-09 21:18:13 -0800

    created user schema/model and created CRUD operation methods

---
+++ src/db/users.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+// Creating a new user model
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false }
+  }
+});
+
+// Converting the model to a schema in the database
+export const UserModel = mongoose.model('User', UserSchema);
+
+// Creating methods to perform CRUD operations easily
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
+    'authentication.sessionToken': sessionToken,
+});
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);

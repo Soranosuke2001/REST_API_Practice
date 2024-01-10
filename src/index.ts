fae8dc033cd2a8c1851b8fb3a9f76ad967fdcd7e
Soranosuke2001 <sorazora1.4@gmail.com>
2024-01-09 21:07:19 -0800

    setup mongoDB connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -20,3 +23,8 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log("Server running on http://localhost:8080/");
 });
+
+// Connecting to the MongoDB
+mongoose.Promise = Promise;
+mongoose.connect(`${process.env.MONGO_URL}`);
+mongoose.connection.on('error', (error: Error) => console.log(error));
